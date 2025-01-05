cf03e916e3e92cf0e90073113299ff930e95ce59
dwinfield23 <155680806+dwinfield23@users.noreply.github.com>
2025-01-05 16:36:40 -0600

    Update weather.js

--- src/weather.js
+++ src/weather.js
@@ -1,1 +1,26 @@
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let city = searchInputElement.value;
+
+  let apiKey = "da335f13db1db46eo138ac1a2af9tbe0";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+
+  axios.get(apiUrl).then(changeWeather);
+}
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
 
